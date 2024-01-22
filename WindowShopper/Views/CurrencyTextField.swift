ea4a78bf2bcfa1ebd810fd8e0b5d9711cb2272c7
Atharva Sakpal <atharvasakpal14@gmail.com>
2024-01-22 16:12:15 +0530

    lesson1 done ig

--- WindowShopper/Views/CurrencyTextField.swift
+++ WindowShopper/Views/CurrencyTextField.swift
@@ -9,12 +9,30 @@ import UIKit
 
 class CurrencyTextField: UITextField {
 
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
+    override func awakeFromNib() {
+        //awakeFromNib -Prepares the receiver for service after it has been loaded from an Interface Builder archive, or nib file.
+        super.awakeFromNib()
+        backgroundColor = UIColor(red: 255.0/255.0, green: 255.0/255.0, blue: 255.0/255.0 , alpha: 0.25) //white
+        
+        layer.cornerRadius = 5.0
+        textAlignment = .center
+        
+        if placeholder == nil{
+            placeholder  = " "
+        }
+        
+        if let p = placeholder{
+            
+            let place = NSAttributedString(string: p, attributes: [.foregroundColor:UIColor(red: 255.0/255.0, green: 255.0/255.0, blue: 255.0/255.0 , alpha: 1.0)])
+            
+            attributedPlaceholder = place
+            textColor = UIColor(red: 255.0/255.0, green: 255.0/255.0, blue: 255.0/255.0 , alpha: 1.0) //white
+            
+            //NSAttributedString - An NSAttributedString object manages character strings and associated sets of attributes (for example, font and kerning) that apply to individual characters or ranges of characters in the string. An association of characters and their attributes is called an attributed string. The cluster’s two public classes, NSAttributedString and NSMutableAttributedString, declare the programmatic interface for read-only attributed strings and modifiable attributed strings, respectively.
+            
+            
+        }
+        
     }
-    */
 
 }
