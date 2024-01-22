d89e09705e36724987d8550eb83015bacf0ea022
Atharva Sakpal <atharvasakpal14@gmail.com>
2024-01-23 01:05:52 +0530

    lesson02 completed

--- WindowShopper/Controller/MainViewController.swift
+++ WindowShopper/Controller/MainViewController.swift
@@ -9,9 +9,32 @@ import UIKit
 
 class MainViewController: UIViewController {
 
+   
+    @IBOutlet weak var priceTxt: UITextField!
+    
+    @IBOutlet weak var wageTxt: CurrencyTextField!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        
+        //making a UIbutton manually (Calculate button)
+        let calcBtn =  UIButton(frame: CGRect(x: 0, y: 0, width: view.frame.size.width, height: 60))
+        
+        calcBtn.backgroundColor = UIColor(red: 255.0/255/0, green: 140.0/255.0, blue: 0.0/255.0, alpha: 1.0)
+        calcBtn.setTitle("Calculate", for: .normal) //always use setTitle instead of title
+        calcBtn.setTitleColor(UIColor(red: 255.0/255/0, green: 255.0/255.0, blue: 255.0/255.0, alpha: 1.0), for: .normal)
+        calcBtn.addTarget(self, action: #selector(MainViewController.calculate), for: .touchUpInside)
+        
+        
+        wageTxt.inputAccessoryView = calcBtn
+        priceTxt.inputAccessoryView = calcBtn
+        
+    }
+    
+    
+    @objc func calculate(){
+        print("We got here")
     }
 
 
--- WindowShopper/Views/CurrencyTextField.swift
+++ WindowShopper/Views/CurrencyTextField.swift
@@ -7,32 +7,46 @@
 
 import UIKit
 
+@IBDesignable //to see the changes done programatically in the main storyboard as well
 class CurrencyTextField: UITextField {
 
+    
+    
+    
+    //override func prepareForInterfaceBuilder() {
+        //customizeView()
+    //}
+    
     override func awakeFromNib() {
         //awakeFromNib -Prepares the receiver for service after it has been loaded from an Interface Builder archive, or nib file.
         super.awakeFromNib()
-        backgroundColor = UIColor(red: 255.0/255.0, green: 255.0/255.0, blue: 255.0/255.0 , alpha: 0.25) //white
+        //customizeView()
         
-        layer.cornerRadius = 5.0
-        textAlignment = .center
         
-        if placeholder == nil{
-            placeholder  = " "
-        }
+            backgroundColor = UIColor(red: 255.0/255.0, green: 255.0/255.0, blue: 255.0/255.0 , alpha: 0.25) //white,  alpha ~~ opacity
+            
+            layer.cornerRadius = 5.0
+            textAlignment = .center
+            
+            if placeholder == nil{
+                placeholder  = " "
+            }
+            
+            if let p = placeholder{
+                
+                let place = NSAttributedString(string: p, attributes: [.foregroundColor:UIColor(red: 255.0/255.0, green: 255.0/255.0, blue: 255.0/255.0 , alpha: 1.0)])
+                
+                attributedPlaceholder = place
+                textColor = UIColor(red: 255.0/255.0, green: 255.0/255.0, blue: 255.0/255.0 , alpha: 1.0) //white
+                
+                //NSAttributedString - An NSAttributedString object manages character strings and associated sets of attributes (for example, font and kerning) that apply to individual characters or ranges of characters in the string. An association of characters and their attributes is called an attributed string. The cluster’s two public classes, NSAttributedString and NSMutableAttributedString, declare the programmatic interface for read-only attributed strings and modifiable attributed strings, respectively.
+            }
         
-        if let p = placeholder{
-            
-            let place = NSAttributedString(string: p, attributes: [.foregroundColor:UIColor(red: 255.0/255.0, green: 255.0/255.0, blue: 255.0/255.0 , alpha: 1.0)])
-            
-            attributedPlaceholder = place
-            textColor = UIColor(red: 255.0/255.0, green: 255.0/255.0, blue: 255.0/255.0 , alpha: 1.0) //white
-            
-            //NSAttributedString - An NSAttributedString object manages character strings and associated sets of attributes (for example, font and kerning) that apply to individual characters or ranges of characters in the string. An association of characters and their attributes is called an attributed string. The cluster’s two public classes, NSAttributedString and NSMutableAttributedString, declare the programmatic interface for read-only attributed strings and modifiable attributed strings, respectively.
-            
-            
-        }
+        
+        
         
     }
+    
+    
 
 }
