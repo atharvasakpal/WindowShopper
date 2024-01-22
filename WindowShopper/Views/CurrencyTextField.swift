12b29c0939a20a54b2f0e1b8850d86fc47a2f921
Atharva Sakpal <atharvasakpal14@gmail.com>
2024-01-22 16:08:48 +0530

    lesson 01 done

---
+++ WindowShopper/Views/CurrencyTextField.swift
@@ -0,0 +1,20 @@
+//
+//  CurrencyTextField.swift
+//  WindowShopper
+//
+//  Created by Atharva Sakpal on 22/01/24.
+//
+
+import UIKit
+
+class CurrencyTextField: UITextField {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
